2efa35cd2df1181f31a389d4dbf4f44e54979383
m-tsuru <m-tsuru@users.noreply.github.com>
2024-05-21 08:28:34 +0000

    Apply Prettier Change

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,58 +1,56 @@
 export const Header = () => {
-    return(
+    return (
         <>
-        <header>
-			<div class="header">
-				<div class="header left">
-			
-				</div>
-				<nav class="header-right">
-					<ul class="nav-list">
-						<a href="./home.html">
-							<li>HOME</li>
-						</a>
-						<a href="./stage.html">
-							<li>STAGE</li>
-						</a>
-						<a href="./map.html">
-							<li>MAP</li>
-						</a>
-						<a href="./about.html">
-							<li>ABOUT</li>
-						</a>
-						<a href="./access.html">
-							<li>ACCESS</li>
-						</a>
-					</ul>
+            <header>
+                <div class="header">
+                    <div class="header left"></div>
+                    <nav class="header-right">
+                        <ul class="nav-list">
+                            <a href="./home.html">
+                                <li>HOME</li>
+                            </a>
+                            <a href="./stage.html">
+                                <li>STAGE</li>
+                            </a>
+                            <a href="./map.html">
+                                <li>MAP</li>
+                            </a>
+                            <a href="./about.html">
+                                <li>ABOUT</li>
+                            </a>
+                            <a href="./access.html">
+                                <li>ACCESS</li>
+                            </a>
+                        </ul>
 
-					<div class="inner">
-						<ul>
-							<a href="./home.html">
-								<li>HOME</li>
-							</a>
-							<a href="./stage.html">
-								<li>STAGE</li>
-							</a>
-							<a href="./map.html">
-								<li>MAP</li>
-							</a>
-							<a href="./about.html">
-								<li>ABOUT</li>
-							</a>
-							<a href="./access.html">
-								<li>ACCESS</li>
-							</a>
-						</ul>
-					</div>
-				</nav>
-				<div class="toggle-btn">
-					<span></span>
-					<span></span>
-					<span></span>
-				</div>
-				<div id="mask"></div>
-			</div>
-		</header>
+                        <div class="inner">
+                            <ul>
+                                <a href="./home.html">
+                                    <li>HOME</li>
+                                </a>
+                                <a href="./stage.html">
+                                    <li>STAGE</li>
+                                </a>
+                                <a href="./map.html">
+                                    <li>MAP</li>
+                                </a>
+                                <a href="./about.html">
+                                    <li>ABOUT</li>
+                                </a>
+                                <a href="./access.html">
+                                    <li>ACCESS</li>
+                                </a>
+                            </ul>
+                        </div>
+                    </nav>
+                    <div class="toggle-btn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div id="mask"></div>
+                </div>
+            </header>
         </>
-    )
-}
+    );
+};
