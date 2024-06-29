655521126d8891642f538b9e9119a7d4bb8ce323
nagomi1529 <i20023@e.hiroshima-cu.ac.jp>
2024-06-29 17:10:36 +0900

    footerを追加しました

---
+++ src/components/footer.jsx
@@ -0,0 +1,35 @@
+export const footer = () => {
+    return(
+        <>
+        <footer>
+            <div class="container footer-content mt-5 mb-5">
+                <div class="row">
+                    <div class="col-md-6">
+                    <p class="h5">問い合わせ先</p>
+                    <p class="h4">広島市立大学 大学祭実行委員会</p>
+                    <p>
+                    <span class="d-inline-block">〒 731-3194</span>
+                    <span class="d-inline-block">広島市安佐南区大塚東三丁目4番1号</span>
+                    <span class="d-inline-block">学生会館 2階 大祭室</span>
+                    <span class="d-block">TEL & FAX:
+                        <a href="tel:0828485443" style="color: black">(082) 848-5443</a>
+                    </span>
+                    <span class="d-block">電子メール: ichidaisai@outlook.jp</span>
+                    </p>
+                    <p>&copy; 2024 広島市立大学 大学祭実行委員会</p>
+                </div>
+                <div class="col">
+                    <p class="h4">SNS で実行委員会をフォロー</p>
+                    <a href="#" style="text-decoration: none;">
+						<img class="sns-icon" src="./img/x_icon.png" alt="ichidaisai-x"></img>
+					</a>
+					<a href="#" style="text-decoration: none;">
+						<img class="sns-icon" src="./img/instagram_icon.png" alt="ichidaisai-instagram"></img>
+                    </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        </>
+    )
+}
