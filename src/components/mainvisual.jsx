3e092ceb99f53bf1400d43fbc81376f15ede5d25
nagomi1529 <i20023@e.hiroshima-cu.ac.jp>
2024-08-03 02:20:06 +0900

    日付を修正しました

--- src/components/mainvisual.jsx
+++ src/components/mainvisual.jsx
@@ -3,7 +3,7 @@ export const mainvisual = () => {
         <>
         <title>広島市立大学 第31回 大学祭</title>
         <img className="home-visual"src="./img/header.png" alt=""></img>
-        <h2 className="mainvisual">{'31st \nICHIDAISAI \n10/27(sat.) 10/28(sun.) 10:00 ~ 20:00'}</h2>
+        <h2 className="mainvisual">{'31st \nICHIDAISAI \n10/26 - 10/27  10:00 ~ 20:00'}</h2>
         </>
     );
 };
