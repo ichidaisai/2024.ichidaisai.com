d8583e8f012901bfa01c1fe38169926588161c8a
nagomi1529 <nagomi1529@users.noreply.github.com>
2024-08-03 12:41:13 +0000

    Apply Prettier Change

--- src/components/Mainvisual.jsx
+++ src/components/Mainvisual.jsx
@@ -1,9 +1,11 @@
 export const mainvisual = () => {
-    return(
+    return (
         <>
-        <title>広島市立大学 第31回 大学祭</title>
-        <img className="home-visual"src="./img/header.png" alt=""></img>
-        <h2 className="mainvisual">{'31st \nICHIDAISAI \n10/26 - 10/27  10:00 ~ 20:00'}</h2>
+            <title>広島市立大学 第31回 大学祭</title>
+            <img className="home-visual" src="./img/header.png" alt=""></img>
+            <h2 className="mainvisual">
+                {"31st \nICHIDAISAI \n10/26 - 10/27  10:00 ~ 20:00"}
+            </h2>
         </>
     );
 };
