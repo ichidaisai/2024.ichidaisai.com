c25f33860f06fd98c4e122c557b73bb71d0a1bb7
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-08-03 22:26:10 +0900

    Add: CSS of mainvisual

--- src/components/Mainvisual.jsx
+++ src/components/Mainvisual.jsx
@@ -1,10 +1,16 @@
+import "../styles/Mainvisual.css";
+
 export const Mainvisual = () => {
     return (
         <>
             <title>広島市立大学 第31回 大学祭</title>
             <img className="home-visual" src="./img/header.png" alt=""></img>
-            <h2 className="mainvisual">
-                {"31st \nICHIDAISAI \n10/26 - 10/27  10:00 ~ 20:00"}
+            <h2 className="datetime">
+                <span>31st</span><br />
+                <span className="colored">I</span>CHIDAISAI<br />
+                <span className="colored">F</span>ESTIVAL<br />
+                <span className="colored">10/26(Sat.), 10/27(Sun.)</span><br />
+                <span className="colored">10:00 ~ 20:00</span>
             </h2>
         </>
     );
