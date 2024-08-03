353164e447e5ed29e0f4a64bf57a37d8ed6d3f6f
hina81 <hina81@users.noreply.github.com>
2024-08-03 02:36:51 +0000

    Apply Prettier Change

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,24 +1,28 @@
 import { Link } from "react-router-dom";
 
 export const NewsList = () => {
-    return(
+    return (
         <>
-        <div className="news">
-            <ul className="news-list">
-                <li class="item">
-                    <a href="#guest">
-                        <p className="date">2024/8/4</p>
-                        <p className="title">今年のゲストはArakezuriに決定！</p>
-                    </a>
-                </li>
-                <li class="item">
-                    <Link href="#">
-                        <p className="date">2024/6/28</p>
-                        <p className="title">現在準備中です！随時更新しますのでもうしばらくお待ちください。</p>
-                    </Link>
-                </li>
-            </ul>
-        </div>
+            <div className="news">
+                <ul className="news-list">
+                    <li class="item">
+                        <a href="#guest">
+                            <p className="date">2024/8/4</p>
+                            <p className="title">
+                                今年のゲストはArakezuriに決定！
+                            </p>
+                        </a>
+                    </li>
+                    <li class="item">
+                        <Link href="#">
+                            <p className="date">2024/6/28</p>
+                            <p className="title">
+                                現在準備中です！随時更新しますのでもうしばらくお待ちください。
+                            </p>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </>
-    )
-}
+    );
+};
