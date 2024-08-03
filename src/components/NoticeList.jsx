22898650d1510d5b613dd5372b4607f0f0f2227d
m-tsuru <m-tsuru@users.noreply.github.com>
2024-08-03 13:01:14 +0000

    Apply Prettier Change

--- src/components/NoticeList.jsx
+++ src/components/NoticeList.jsx
@@ -1,23 +1,21 @@
-
-
 export const Pending = () => {
     return (
-     <>
-        <div className="tyuuizikou">
-            <h2 className="section-title">注意事項</h2>
-            <h3>大学祭に来られる際の注意事項は以下になります．</h3>
-            <p>
-                ☆全面禁煙
-                <br></br>
-                ☆全面禁酒、持ち込みも不可
-                <br></br>
-                ☆ペット禁止
-                <br></br>
-                ☆感染症予防のお願い
-                <br></br>
-                ☆公共交通機関にてお越しください
-            </p>
-        </div>
-     </>
+        <>
+            <div className="tyuuizikou">
+                <h2 className="section-title">注意事項</h2>
+                <h3>大学祭に来られる際の注意事項は以下になります．</h3>
+                <p>
+                    ☆全面禁煙
+                    <br></br>
+                    ☆全面禁酒、持ち込みも不可
+                    <br></br>
+                    ☆ペット禁止
+                    <br></br>
+                    ☆感染症予防のお願い
+                    <br></br>
+                    ☆公共交通機関にてお越しください
+                </p>
+            </div>
+        </>
     );
 };
