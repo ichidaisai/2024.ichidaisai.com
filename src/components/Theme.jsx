48a055af13f177fa916905b0930983ab4d4134ff
NANAPC\na10n <h20198@e.hiroshima-cu.ac.jp>
2024-08-04 00:48:02 +0900

    Theme.cssを追加、それに合わせてclassName変更

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,11 +1,11 @@
+import "../styles/Theme.css";
 export const Theme = () => {
     return(
         <>
             <div className="Theme">
-            <div className="theme-sectiton">
-                <h2 className="section-title">THEME</h2>
-                <div className="theme-content sub-content-width">
-                    <img className="ichidaisai-mainvisual sub-content-width" src="./img/poster.png" alt="">
+                <h2 className="section-title"><span>T</span>HEME</h2>
+                <div className="theme-content">
+                    <img className="ichidaisai-mainvisual" src="./img/poster.png" alt="">
                     </img>
                     <h2>Spawn！（スポーン）</h2>
                     <p>
@@ -14,7 +14,6 @@ export const Theme = () => {
                         また、英語のスラングとして「ゲームを開始する」「ゲーム内のアイテムが登場する」という意味を持ちます。大学祭が始まるまでの緊張感といざ始まるぞ！というワクワク感、ゲームや大学祭が始まったらどんなことが起こるのかわからないところはゲームをしている時に似ていて、その感情をみんなで共有できるテーマ案です。
                     </p>
                 </div>
-            </div>
             </div>
         </>  
     );
