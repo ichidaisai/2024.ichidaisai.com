222b18e6caa2d07f91904ff685f1331d520ab837
7na7na3mi <7na7na3mi@users.noreply.github.com>
2024-08-03 13:31:58 +0000

    Apply Prettier Change

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,17 +1,20 @@
 export const Theme = () => {
-    return(
+    return (
         <div className="Theme">
-        <div className="theme-sectiton">
-            <h2 className="section-title">TEHME</h2>
-            <div className="theme-content sub-content-width">
-                <img className="ichidaisai-mainvisual sub-content-width" src="./img/poster.png" alt="">
-                </img>
-                <h2>Spawn！（スポーン）</h2>
-                <p>
-                    動詞としては、引き起こす、生じる、発生させると言う意味があります。大学祭を通してさまざまな人間関係や化学反応が起こればいいなという願いを込めました。また、英語のスラングとして「ゲームを開始する」「ゲーム内のアイテムが登場する」という意味を持ちます。大学祭が始まるまでの緊張感といざ始まるぞ！というワクワク感、ゲームや大学祭が始まったらどんなことが起こるのかわからないところはゲームをしている時に似ていて、その感情をみんなで共有できるテーマ案です。
-                </p>
+            <div className="theme-sectiton">
+                <h2 className="section-title">TEHME</h2>
+                <div className="theme-content sub-content-width">
+                    <img
+                        className="ichidaisai-mainvisual sub-content-width"
+                        src="./img/poster.png"
+                        alt=""
+                    ></img>
+                    <h2>Spawn！（スポーン）</h2>
+                    <p>
+                        動詞としては、引き起こす、生じる、発生させると言う意味があります。大学祭を通してさまざまな人間関係や化学反応が起こればいいなという願いを込めました。また、英語のスラングとして「ゲームを開始する」「ゲーム内のアイテムが登場する」という意味を持ちます。大学祭が始まるまでの緊張感といざ始まるぞ！というワクワク感、ゲームや大学祭が始まったらどんなことが起こるのかわからないところはゲームをしている時に似ていて、その感情をみんなで共有できるテーマ案です。
+                    </p>
+                </div>
             </div>
         </div>
-    </div>  
     );
 };
