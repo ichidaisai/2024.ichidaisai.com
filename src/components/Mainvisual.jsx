f70c67ddbd0dcd9d099d01049d656f9bee3b0f41
nagomi1529 <i20023@e.hiroshima-cu.ac.jp>
2024-08-03 21:37:58 +0900

    Mainvisual.jsx追加しました

---
+++ src/components/Mainvisual.jsx
@@ -0,0 +1,9 @@
+export const mainvisual = () => {
+    return(
+        <>
+        <title>広島市立大学 第31回 大学祭</title>
+        <img className="home-visual"src="./img/header.png" alt=""></img>
+        <h2 className="mainvisual">{'31st \nICHIDAISAI \n10/26 - 10/27  10:00 ~ 20:00'}</h2>
+        </>
+    );
+};
