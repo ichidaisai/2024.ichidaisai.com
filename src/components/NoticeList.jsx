aba85f216aa244559ecff80cc8d99aabc8d63c2f
PoPodada <goito.722@icloud.com>
2024-08-15 13:25:09 +0900

    notice-list修正

--- src/components/NoticeList.jsx
+++ src/components/NoticeList.jsx
@@ -1,10 +1,12 @@
+import "../styles/NoticeList.css"
+
 export const NoticeList = () => {
     return (
         <>
             <div className="tyuuizikou">
                 <h2 className="section-title">注意事項</h2>
                 <p>大学祭に来られる際の注意事項は以下になります．</p>
-                <ul>
+                <ul className="notice-list">
                     <li>敷地内全面禁煙</li>
                     <li>敷地内での飲酒禁止</li>
                     <li>敷地内への酒類の持込禁止</li>
