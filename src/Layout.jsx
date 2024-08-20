e8c119153c63e7466d9ab73b124e8ab70fbfed70
m-tsuru <m-tsuru@users.noreply.github.com>
2024-08-20 08:09:51 +0000

    Apply Prettier Change

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = () => {
     return (
         <>
             <main>
-            <Header />
+                <Header />
                 <Outlet />
             </main>
             <Footer />
