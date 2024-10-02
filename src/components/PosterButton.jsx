338bf52655d59904215cd1e30b16f09ced4a6dc9
hina81 <hina81@users.noreply.github.com>
2024-10-02 03:49:19 +0000

    Apply Prettier Change

--- src/components/PosterButton.jsx
+++ src/components/PosterButton.jsx
@@ -10,7 +10,7 @@ export const PosterButton = () => {
                     市大祭の宣伝ポスターの掲示にご協力をいただいた施設や店舗の皆様をご紹介します！
                     各所に掲示されたポスターをぜひご覧いただき、大学祭の魅力を感じ取ってください。
                 </p>
-                <Link style={{ textDecoration: 'none' }} to="/poster">
+                <Link style={{ textDecoration: "none" }} to="/poster">
                     <button className="btn">
                         ポスター掲示場所一覧はこちら
                     </button>
