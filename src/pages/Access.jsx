2365e11349a15856994cf57fc1d6c0b211b668dd
yoshidaasato <akkunn1026@gmail.com>
2024-10-05 00:20:19 +0900

    add_access_page

--- src/pages/Access.jsx
+++ src/pages/Access.jsx
@@ -5,9 +5,23 @@ export const Access = () => {
         <div className="access">
             <div className="text">
                 <h2>アクセス</h2>
-                <h3>アストラムライン</h3>
+                
+                <h3>
+                    <span className="way-span">
+                        アストラムライン
+                    </span>
+                    
+                </h3>
+
                 <p>大塚駅下車 徒歩２５分</p>
-                <h3>バス</h3>
+
+                <h3>
+                    <span className="way-span">
+                        バス
+                    </span>
+                    
+                </h3>
+
                 <p>
                     広島バスセンターより
                     <br />
@@ -29,7 +43,7 @@ export const Access = () => {
                 <p><a href="https://www.hiroshima-cu.ac.jp/uploads/2023/10/02180f5651c14ec6aeed073782036045-20231011003039821.pdf">PDFはこちらから</a></p>
             </div>
             <div className="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12888.087419815736!2d132.4186049951159!3d34.43753606139676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355a9846b8b10471%3A0xb50adccc4687ad83!2z5bqD5bO25biC56uL5aSn5a2m!5e1!3m2!1sja!2sjp!4v1727829365634!5m2!1sja!2sjp" width={800} height={600}></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12888.087419815736!2d132.4186049951159!3d34.43753606139676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355a9846b8b10471%3A0xb50adccc4687ad83!2z5bqD5bO25biC56uL5aSn5a2m!5e1!3m2!1sja!2sjp!4v1727829365634!5m2!1sja!2sjp"className="gmap_iframe" allowfullscreen=""></iframe>
             </div>
         </div>
     );
