e3c6e467d3a0f43cd9daf8e4b6719758cce72804
hina81 <i20203@e.hiroshima-cu.ac.jp>
2024-10-12 12:58:01 +0900

    ハンバーガーメニューの作り直し

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import "../styles/Header.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import ichidaisai_logo_512 from "../assets/ichidaisai_logo_512.png";
+import { SlArrowRight } from "react-icons/sl";
 
 export const Header = () => {
     const [open, setOpen] = useState(false);
@@ -8,80 +10,117 @@ export const Header = () => {
         setOpen(!open);
     };
     return (
-        <>
-            <header>
-                <img
-                    src={ichidaisai_logo_512}
-                    alt="header-logo"
-                    className="header-logo"
-                />
-                <nav>
-                    <ul className="nav-list">
+        <header>
+            <img
+                src={ichidaisai_logo_512}
+                alt="header-logo"
+                className="header-logo"
+            />
+
+            <nav>
+                <ul className={`nav-list ${open ? "open" : ""}`}>
+                    <Link to="/">
+                        <li>HOME</li>
+                    </Link>
+                    <Link to="/guest">
+                        <li>GUEST</li>
+                    </Link>
+                    <Link to="/pending">
+                        <li>STAGE</li>
+                    </Link>
+                    <Link to="/pending">
+                        <li>MAP</li>
+                    </Link>
+                    <Link to="/pending">
+                        <li>ABOUT</li>
+                    </Link>
+                    <Link to="/pending">
+                        <li>ACCESS</li>
+                    </Link>
+                    <Link to="/funding">
+                        <li>FUNDING</li>
+                    </Link>
+                </ul>
+
+                <div
+                    className={`toggle_btn ${open ? "active" : ""}`}
+                    onClick={handleOpen}
+                    id="btn"
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <div
+                    className={`nav-menu ${
+                        open ? "active-nav-list" : "none-nav-list"
+                    }`}
+                >
+                    <ul>
                         <Link to="/">
-                            <li>HOME</li>
+                            <li>
+                                <span>HOME</span>
+                                <span className="right">
+                                    <SlArrowRight />
+                                </span>
+                            </li>
                         </Link>
                         <Link to="/guest">
-                            <li>GUEST</li>
+                            <li>
+                                <span className="left">GUEST</span>
+                                <span className="right">
+                                    <SlArrowRight />
+                                </span>
+                            </li>
                         </Link>
                         <Link to="/stage">
-                            <li>STAGE</li>
+                            <li>
+                                <span className="left">STAGE</span>
+                                <span className="right">
+                                    <SlArrowRight />
+                                </span>
+                            </li>
                         </Link>
                         <Link to="/pending">
-                            <li>MAP</li>
+                            <li>
+                                <span className="left">MAP</span>
+                                <span className="right">
+                                    <SlArrowRight />
+                                </span>
+                            </li>
                         </Link>
                         <Link to="/pending">
-                            <li>ABOUT</li>
+                            <li>
+                                <span className="left">ABOUT</span>
+                                <span className="right">
+                                    <SlArrowRight />
+                                </span>
+                            </li>
                         </Link>
                         <Link to="/access">
-                            <li>ACCESS</li>
+                            <li>
+                                <span className="left">ACCESS</span>
+                                <span className="right">
+                                    <SlArrowRight />
+                                </span>
+                            </li>
                         </Link>
                         <Link to="/funding">
-                            <li>FUNDING</li>
+                            <li>
+                                <span className="left">FUNDING</span>
+                                <span className="right">
+                                    <SlArrowRight />
+                                </span>
+                            </li>
                         </Link>
                     </ul>
-                    <button
-                        className={`openbtn ${open && "active"}`}
-                        onClick={handleOpen}
-                    >
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </button>
-                    <div
-                        className={`${
-                            open ? "active-nav-list" : "none-nav-list"
-                        }`}
-                    >
-                        <ul>
-                            <Link to="/">
-                                <li>HOME</li>
-                            </Link>
-                            <Link to="/guest">
-                                <li>GUEST</li>
-                            </Link>
-                            <Link to="/stage">
-                                <li>STAGE</li>
-                            </Link>
-                            <Link to="/pending">
-                                <li>MAP</li>
-                            </Link>
-                            <Link to="/pending">
-                                <li>ABOUT</li>
-                            </Link>
-                            <Link to="/access">
-                                <li>ACCESS</li>
-                            </Link>
-                            <Link to="/funding">
-                                <li>FUNDING</li>
-                            </Link>
-                        </ul>
-                    </div>
-                    <div
-                        className={`${open ? "mask" : "mask-none"}`}
-                        onClick={handleOpen}
-                    ></div>
-                </nav>
-            </header>
-        </>
+                </div>
+                <div
+                    className={`${open ? "mask" : "mask-none"}`}
+                    onClick={handleOpen}
+                ></div>
+            </nav>
+        </header>
     );
 };
