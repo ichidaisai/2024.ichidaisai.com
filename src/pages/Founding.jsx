a2492c9417b7b3cbd1eeb29ca5bc94beb3e1722e
KANNJI\sumik <h20112@e.hiroshima-cu.ac.jp>
2024-10-17 17:28:21 +0900

    クラファンページの変更を改めて追加します

--- src/pages/Founding.jsx
+++ src/pages/Founding.jsx
@@ -8,8 +8,9 @@ import ichi3 from '../assets/ichipoppo/106003.png'
 export const Funding = () => {
     return (
         <div className="funding">
+        <h2>クラウドファンディングに</h2>
+        <h3>挑戦します！</h3>
             <p>
-                クラウドファンディングに挑戦します！
                 <br />
                 今年初の試みとして、市大祭はより一層の盛り上がりを目指して、クラウドファンディングに挑戦します！
                 <br />
@@ -27,7 +28,7 @@ export const Funding = () => {
             >
                 <button className="btn">クラファンページはこちら</button>
             </Link>
-            <p>
+            <a>
                 クラウドファンディングで市大祭を支援し、外部ゲストを招きませんか？！
                 <br />
                 今年の大学祭のテーマは『Spawn!』です。
@@ -39,7 +40,7 @@ export const Funding = () => {
                 しかし、運営資金はOBOG（卒業生）後援会や企業様からの協賛金に支えられているものの、新たな取り組みが多い今年は特に資金面での課題があります。
                 <br />
                 31年目を迎える市大祭を成功させ、これからも長く続く大学祭を作り上げるために、ぜひ皆さまのご支援をお願いいたします。
-            </p>
+            </a>
 
             <div className="ichipoppo-funding">
                 <div>
@@ -53,5 +54,6 @@ export const Funding = () => {
                 </div>
             </div>
         </div>
+        
     )
 }
