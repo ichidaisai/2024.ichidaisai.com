de128837766d234f306b0da87ca196f9164848f9
Gutchii <yama.kguyama@outlook.jp>
2024-10-20 15:24:15 +0900

    modifieddesign

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import xLogo from "../assets/x_logo.png";
+import xLogo from "../assets/logo-white.png";
 import instagramLogo from "../assets/Instagram_Glyph_White.png";
 
 export const Footer = () => {
