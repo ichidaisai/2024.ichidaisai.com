384e2141695beac85475f68de7a9167572b80338
PoPodada <PoPodada@users.noreply.github.com>
2024-10-23 03:26:29 +0000

    Apply Prettier Change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
-import xLogo from "../assets/logo-white.png";
-import instagramLogo from "../assets/Instagram_Glyph_White.png";
+import { Link } from 'react-router-dom'
+import xLogo from '../assets/logo-white.png'
+import instagramLogo from '../assets/Instagram_Glyph_White.png'
 
 export const Footer = () => {
     return (
