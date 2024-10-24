73415423209f90127267f68abae01b0fdf184a44
KANNJI\sumik <h20112@e.hiroshima-cu.ac.jp>
2024-10-24 16:46:33 +0900

    /funding/collabguestに変更しました

--- src/main.jsx
+++ src/main.jsx
@@ -52,7 +52,7 @@ const router = createBrowserRouter([
                 element: <StampRally />,
             },
             {
-                path: '/kuragest',
+                path: '/funding/collabguest',
                 element: <Kuragest />,
             },
         ],
