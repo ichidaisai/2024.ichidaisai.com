a81c33b976cf79b31cf56a8317d1fbce60bd06e3
yoshidaasato <yoshidaasato@users.noreply.github.com>
2024-11-11 08:01:28 +0000

    Apply Prettier Change

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -4,17 +4,16 @@ import stage_background from '../assets/stage_background.webp'
 export const End = () => {
     return (
         <>
-
-                <h2 className='section-title'>第31回市大祭は終了いたしました。</h2>
-                <div className='end'>    
-                    <div className='content'>
+            <h2 className="section-title">第31回市大祭は終了いたしました。</h2>
+            <div className="end">
+                <div className="content">
                     <p>
-                    10月26日、27日に行われた第31回市大祭は無事終了いたしました。
-                    <br />
-                    たくさんのご来場、温かいご支援、そしてご協力ありがとうございました！
+                        10月26日、27日に行われた第31回市大祭は無事終了いたしました。
+                        <br />
+                        たくさんのご来場、温かいご支援、そしてご協力ありがとうございました！
                     </p>
                 </div>
-                <div className='picture'>
+                <div className="picture">
                     <img src={stage_background} />
                 </div>
             </div>
