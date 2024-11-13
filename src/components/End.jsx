d44e38918ef21f6270dcb5830c1f4715cd7555c6
nagomi1529 <i20023@e.hiroshima-cu.ac.jp>
2024-11-13 12:18:11 +0900

    クラファンページに遷移するリンクを追加しました

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom'
 import '../styles/End.css'
 import stage_background from '../assets/stage_background.webp'
+
+const returnTop = () => {
+    window.scrollTo({
+        top: 0,
+    })
+}
 
 export const End = () => {
     return (
@@ -12,6 +19,14 @@ export const End = () => {
                         <br />
                         たくさんのご来場、温かいご支援、そしてご協力ありがとうございました！
                     </p>
+                    <br />
+                    <Link 
+                    className="switch-funding"
+                    to="funding"
+                    onClick={returnTop}
+                    >
+                        <h5>クラウドファンディング結果ページはこちら</h5>
+                    </Link>
                 </div>
                 <div className="picture">
                     <img src={stage_background} />
