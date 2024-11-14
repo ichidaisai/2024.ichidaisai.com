c1bc8b530c1190f78b91cfb86f555be422e78543
cldbzl <cldbzl@users.noreply.github.com>
2024-11-14 03:55:59 +0000

    Apply Prettier Change

--- src/pages/Founding.jsx
+++ src/pages/Founding.jsx
@@ -1,4 +1,4 @@
-import '../styles/Funding.css';
+import '../styles/Funding.css'
 
 import ichipopo1 from '../assets/ichipoppo/ichipopo1.webp'
 import ichipopo2 from '../assets/ichipoppo/ichipopo2.webp'
@@ -8,24 +8,54 @@ import ichipopo4 from '../assets/ichipoppo/ichipopo4.webp'
 const supportersList1 = ['ヨモ']
 const supportersList2 = ['タロウ', 'たか', 'oikawa']
 const supportersList3 = [
-    'ryukun', 'Haitii', 'YOKOYOKO', 'しんちゃん', 
-    'あおいの母ちゃん', 'ギョンス', 'ゆたゆきママ', '画角斜め', '31boss', 
-    'ぱぴこ', '情報科学部長', '31koho-boss', 'まさえちゃん', '統括', 
-    'poco', 'r', 'MY', 'K.T', 'ざっく', 
-    'はんちゃん', 'JpH', 'こあっさん', 'amaちゃん', 'chry'
-];
+    'ryukun',
+    'Haitii',
+    'YOKOYOKO',
+    'しんちゃん',
+    'あおいの母ちゃん',
+    'ギョンス',
+    'ゆたゆきママ',
+    '画角斜め',
+    '31boss',
+    'ぱぴこ',
+    '情報科学部長',
+    '31koho-boss',
+    'まさえちゃん',
+    '統括',
+    'poco',
+    'r',
+    'MY',
+    'K.T',
+    'ざっく',
+    'はんちゃん',
+    'JpH',
+    'こあっさん',
+    'amaちゃん',
+    'chry',
+]
 const supportersList4 = [
-    'いなさん', 'たにぱん', 'そのちゃん', 'キタノ', 'さかもと', 
-    'ヒカル', 'いちはら', 'fish', 'ケン', 'ふふ', 
-    'ひさし', '前川亜里(広報OB)', 'ゆずれもん', 'teko'
-];
+    'いなさん',
+    'たにぱん',
+    'そのちゃん',
+    'キタノ',
+    'さかもと',
+    'ヒカル',
+    'いちはら',
+    'fish',
+    'ケン',
+    'ふふ',
+    'ひさし',
+    '前川亜里(広報OB)',
+    'ゆずれもん',
+    'teko',
+]
 const supportersList5 = ['他50名']
 
 export const Funding = () => {
     return (
         <div className="funding">
             <h2>クラウドファンディング結果報告</h2>
-            
+
             <div className="funding_greeting">
                 <p>
                     今年の市大祭のテーマ「Spawn」には、「化学反応を起こす」という想いを込めています。
@@ -49,7 +79,9 @@ export const Funding = () => {
             <div className="funding_amount_section">
                 <span className="funding_amount_label">集まった金額</span>
                 <span className="funding_amount">617,000 円</span>
-                <span className="funding_amount_label2">(目標金額500,000円)</span>
+                <span className="funding_amount_label2">
+                    (目標金額500,000円)
+                </span>
             </div>
 
             <div className="supporters_count_section">
@@ -66,12 +98,20 @@ export const Funding = () => {
             </div>
 
             <div className="supporters_titles">
-                <div className="supporters_title">Spawn！プロジェクト支援者様一覧</div>
+                <div className="supporters_title">
+                    Spawn！プロジェクト支援者様一覧
+                </div>
                 <div className="supporters_title_caution">(敬称略)</div>
             </div>
 
             <div className="supporters_list">
-                {[supportersList1, supportersList2, supportersList3, supportersList4, supportersList5].map((list, index) => (
+                {[
+                    supportersList1,
+                    supportersList2,
+                    supportersList3,
+                    supportersList4,
+                    supportersList5,
+                ].map((list, index) => (
                     <div key={index} className={`supporters_list${index + 1}`}>
                         {list.map((supporter, idx) => (
                             <div key={idx}>{supporter}</div>
@@ -81,7 +121,7 @@ export const Funding = () => {
             </div>
 
             <div className="funding_lastgreeting">
-                <p>    
+                <p>
                     皆様の温かいご支援と応援に、心より感謝申し上げます。
                     <br />
                     頂いた支援金は、第31回市大祭のオリジナルグッズ制作やステージ企画の運営費として
